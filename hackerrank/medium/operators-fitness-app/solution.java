1b8af4bd99476130d7bea8e7df8b6fcd88ac92f9
GOPINATH R <gopinath2638@gmail.com>
2026-09-09 14:02:33 +0530

    ✅ Operators - Fitness App | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ hackerrank/medium/operators-fitness-app/solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        
+        int timeinsec = (N * 60); 
+        
+        System.out.println(timeinsec);
+        scanner.close();
+    }
+}
