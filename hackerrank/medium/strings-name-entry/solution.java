9bdc3f0e858e90b2e52ff59f87bf3b12d73871cc
GOPINATH R <gopinath2638@gmail.com>
2026-09-23 14:01:00 +0530

    ✅ Strings - Name Entry | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ hackerrank/medium/strings-name-entry/solution.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+ public static void main(String[] args) throws Exception {
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); String f=br.readLine(),l=br.readLine(); System.out.print(f.toLowerCase(Locale.ROOT)+" "+l.toUpperCase(Locale.ROOT));
+ }
+}
