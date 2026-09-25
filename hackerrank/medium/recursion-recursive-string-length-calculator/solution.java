0a4c1c965e0f0dcb7e643cf61d9b74167c28b8d1
GOPINATH R <gopinath2638@gmail.com>
2026-09-25 14:01:44 +0530

    ✅ Recursion - Recursive String Length Calculator | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- hackerrank/medium/recursion-recursive-string-length-calculator/solution.java
+++ hackerrank/medium/recursion-recursive-string-length-calculator/solution.java
@@ -1,5 +1,19 @@
 import java.io.*;
+
 public class Solution {
- static int len(String s,int i){return i==s.length()?0:1+len(s,i+1);}
- public static void main(String[] args)throws Exception{BufferedReader br=new BufferedReader(new InputStreamReader(System.in));String s=br.readLine();if(s==null)s="";System.out.print(len(s,0));}
+    
+    static int len(String s, int i) {
+        return i == s.length() ? 0 : 1 + len(s, i + 1);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        if (s == null) {
+            s = "";
+        }
+        
+        System.out.print(len(s, 0));
+    }
 }
